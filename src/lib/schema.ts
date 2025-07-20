f7eb3f81d523a1ad8ec64a3b7fd20142b79c7d23
Florian Bussmann <hallo@florian-bussmann.de>
2025-07-20 14:36:46 +0000

    Add dialog with annotations

---
+++ src/lib/schema.ts
@@ -0,0 +1,18 @@
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Alert = {
+    fingerprint: string;
+    annotations: {
+        summary: string;
+        description: string;
+    }
+    labels: {
+        alertname: string;
+        instance: string;
+        severity: string;
+    };
+    status: {
+        state: string;
+    };
+};
